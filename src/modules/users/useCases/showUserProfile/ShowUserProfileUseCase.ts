d61a3871e7a2cd0ec9e965a6d63e4c20ba1c856c
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-12 13:39:48 -0300

    Final commit for challenge

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -1,3 +1,5 @@
+import { UsersRepository } from "modules/users/repositories/implementations/UsersRepository";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +11,11 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User does not exist!");
+
+    return user;
   }
 }
 
